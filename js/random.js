b10bfed5b5c58dd0d83ec98a20934eac373395e4
marimultimidia <marimultimidia@gmail.com>
2018-02-03 22:05:04 -0200

    Update random.js

--- js/random.js
+++ js/random.js
@@ -5,21 +5,21 @@
         //1 - person: Kerry Durrant,<br> Green Home Energy Solutions//
         {
 
-        img:"/Users/marimultimidia/Documents/marimultimidia/Templates_Sites_Temas/Food/site/portfolio_mariana/images/testimonial1.png",
+        img:"https://marimultimidia.github.io/portfolio_mariana/images/testimonial1.png",
         text:"‘Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis.’",
         titulo:"Mariana Rodrigues"
         },
         //2 - person: Mark Ryan,<br> Onsite Health Ltd.//
         {
           titulo:"Marta Rodrigues",
-          img:"/Users/marimultimidia/Documents/marimultimidia/Templates_Sites_Temas/Food/site/portfolio_mariana/images/testimonial2.png",
+          img:"https://marimultimidia.github.io/portfolio_mariana/images/testimonial2.png",
           text:"'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean venenatis et lectus nec ullamcorper.'"
         },
         //3 - person: Gareth Chambers,<br> Around Noon Ltd.//
         {
           titulo:"Vitor Andrade",
           text:"'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean venenatis et lectus nec ullamcorper.'",
-          img:"/Users/marimultimidia/Documents/marimultimidia/Templates_Sites_Temas/Food/site/portfolio_mariana/images/testimonial3.png"
+          img:"https://marimultimidia.github.io/portfolio_mariana/images/testimonial3.png"
         }
 
       ];
